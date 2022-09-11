0f48199e963dd06fb6e2754cf3106ca92be1a659
marioavaj <predaj@predaj-tonerov.sk>
2022-09-11 17:13:22 +0200

    xy

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  AfterViewInit,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+  HostListener,
+} from '@angular/core';
 import { Product } from '../models/Product';
 
 @Component({
@@ -6,36 +23,65 @@ import { Product } from '../models/Product';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input() data: Product;
   @Output() reviewAdd: EventEmitter<any> = new EventEmitter<any>();
 
-  reviewFromInput:string;
+  @ViewChild('productPosition') productPosition: ElementRef;
 
-public date = new Date().toLocaleString();//lokalny cas sformatovany
+
+
+  reviewFromInput: string;
+
+  x: number;
+  y: number;
+
+  public date = new Date().toLocaleString(); //lokalny cas sformatovany
 
   constructor() {}
-
-
-
-  ngOnInit(): void {
-
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("zmena doCheck")
+    if (this.productPosition) {
+      this.getPosition();
+    }
+  }
+  ngAfterContentInit(): void {}
+  ngAfterViewChecked(): void {}
+  ngOnDestroy(): void {}
+  ngAfterViewInit(): void {}
+  ngDoCheck(): void {
 
   }
 
-  addReview(review:string){
-    if(this.data){                   //kontrola ci premenna data existuje
-      if(!this.data.reviews){        //kontrola ci v data existuje objekt reviews
-        this.data.reviews = [];       //ak plati podmienka ze neexistuje, vytvori ho
+  ngAfterContentChecked(): void {}
+
+  ngOnInit(): void {}
+
+  addReview(review: string) {
+    if (this.data) {
+      //kontrola ci premenna data existuje
+      if (!this.data.reviews) {
+        //kontrola ci v data existuje objekt reviews
+        this.data.reviews = []; //ak plati podmienka ze neexistuje, vytvori ho
       }
       this.data.reviews.push(review);
       this.reviewAdd.emit(review); // sprava pre parenta ze vlozil recenziu
       this.reviewFromInput = '';
-
-
     }
   }
 
+  getPosition() {
+    this.x = this.productPosition.nativeElement.offsetLeft;
+    this.y = this.productPosition.nativeElement.offsetTop;
+  }
 }
-
-
