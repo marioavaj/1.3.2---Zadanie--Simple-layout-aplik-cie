8e727daf479ffbd93b7e7328e05a0bee33cc0dd7
marioavaj <predaj@predaj-tonerov.sk>
2022-09-25 20:43:57 +0200

    11 uloha na 80%

--- src/app/module-produkt/zoznam-produktov/zoznam-produktov.component.ts
+++ src/app/module-produkt/zoznam-produktov/zoznam-produktov.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/Product';
 import { ProductDetails } from '../../models/Product-details';
+import { SortByPipePipe } from '../sort-by-pipe.pipe';
+
 
 @Component({
   selector: 'app-zoznam-produktov',
@@ -10,16 +12,20 @@ import { ProductDetails } from '../../models/Product-details';
 export class ZoznamProduktovComponent implements OnInit {
   productList: Product[];
   date;
-  sortByClicked: boolean = false;  
+  sortByClicked: boolean;
+ 
   
   lastReview: string;
   filteredData: Product[];
   onStockCheckBox:boolean;
+ 
   constructor() {}
 
   ngOnInit(): void {
    this.productList = ProductDetails.productDetails;
    this.filteredData = this.productList;
+   this.sortByClicked = false;
+   
   }
 
   lastReviewDisplayed(review: any, name:string): void {
@@ -35,7 +41,8 @@ this.onStockCheckBox = onStock;
   }
 
   isClicked(){
-this.sortByClicked = !this.sortByClicked
+this.sortByClicked = !this.sortByClicked;
+    
 console.log(this.sortByClicked);
   }
 }
