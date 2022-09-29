80e277626f00838a2ab397665bb30d1ceb89e176
marioavaj <predaj@predaj-tonerov.sk>
2022-09-29 18:33:49 +0200

    shoping cart

---
+++ src/app/module-produkt/shopping-cart-badge/shopping-cart-badge.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { ShopingCartServiceService } from 'src/app/Services/shoping-cart-service.service';
+
+@Component({
+  selector: 'app-shopping-cart-badge',
+  templateUrl: './shopping-cart-badge.component.html',
+  styleUrls: ['./shopping-cart-badge.component.scss',]
+})
+export class ShoppingCartBadgeComponent implements OnInit {
+  
+  shopingCartItems:any;
+  numberOfItems:number;
+  counter:number;
+
+  constructor(private data: ShopingCartServiceService) { }
+
+  ngOnInit(): void {
+    this.data.dataStream.subscribe((newValue)=>{
+      this.shopingCartItems = newValue;
+      console.log("ikona kosiku \n " + this.shopingCartItems);
+      this.getNumberOfItems(); 
+        
+    console.log(this.counter);
+    });    
+    }
+
+ getNumberOfItems() {
+    this.counter = 0;
+    for (let i = 0; i < this.shopingCartItems.length; i++) {
+        this.counter++;
+        this.numberOfItems = this.counter; 
+    }
+        console.log("pocet poloziek v kosiku  " + this.counter);   
+}
+  
+}
+
