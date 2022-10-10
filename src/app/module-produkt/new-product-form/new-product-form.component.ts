3f42de30324fd27d4eeba96f9135e81f183d5c2f
marioavaj <predaj@predaj-tonerov.sk>
2022-10-10 19:08:41 +0200

    16 uloha 80%

--- src/app/module-produkt/new-product-form/new-product-form.component.ts
+++ src/app/module-produkt/new-product-form/new-product-form.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-product-form',
-  templateUrl: './new-product-form.component.html',
-  styleUrls: ['./new-product-form.component.scss']
+    selector: 'app-new-product-form',
+    templateUrl: './new-product-form.component.html',
+    styleUrls: ['./new-product-form.component.scss'],
 })
 export class NewProductFormComponent implements OnInit {
+    productName = new FormControl('');
+    category = new FormControl('');
+    productPrice = new FormControl('');
+    stockCount = new FormControl('');
+    description = new FormControl('');
+    soldTotal = new FormControl('');
+    soldLastMont = new FormControl('');
+    counter = 7;
 
-title = new FormControl('');
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  createNewProduct(){
-    
-  }
+    ngOnInit(): void {}
+    createNewProduct() {
+        this.counter++;
+        const newproduct = {
+            id: this.counter,
+            name: this.productName.value,
+            category: this.category.value,
+            price: this.productPrice.value,
+            stockCount: this.stockCount.value,
+            sold: this.soldTotal.value,
+            lastMonthSold: this.soldLastMont.value,
+            description: this.description.value,
+            vendors: [],
+            reviews: [],
+        };
+        console.log(newproduct);
+    }
 }
