b8d238d7b5350bb513da45def5ae5cfb40f62396
marioavaj <predaj@predaj-tonerov.sk>
2022-10-17 16:46:14 +0200

    pipe reloaded

--- src/app/module-produkt/sort-by-pipe.pipe.ts
+++ src/app/module-produkt/sort-by-pipe.pipe.ts
@@ -1,57 +1,46 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Product } from '../models/Product';
 
-
 @Pipe({
-  name: 'sortByPipe'
-
-  
+    name: 'sortByPipe',
 })
 export class SortByPipePipe implements PipeTransform {
-  productList:any;
 
-  sortBy: boolean;
-  private cache: any;
-  sortedData:Product[];
 
-  transform(value: Product[], args: boolean): any {
-    if(value !== this.cache){
-      this.cache = this.newValue(value, args)
+    transform(value: Product[], args: boolean): Product[] {
+        if (value) {
+            return this.newValue(value, args);
+        }
+        return [];
     }
-    return this.cache
-  }
-  newValue(productList: Product[], args: boolean ):any{
-   
-   this.productList=productList;
-    if(args === false){
-    function compare( a:Product, b:Product ) {
-     
-      if ( a.name < b.name ){
-        return -1;
-      }
-      if ( a.name > b.name ){
-        return 1;
-      }
-      return 0;
-     }
-    
-    this.sortedData = this.productList.sort( compare);
-    
-  }else {
-    function compare( a:Product, b:Product ) {
-    
-      if ( a.name > b.name ){
-        return -1;
-      }
-      if ( a.name < b.name ){
-        return 1;
-      }
-      return 0;
-     }
-    
-    this.sortedData = this.productList.sort( compare);
-    
-}  
-}
-    
+    newValue(productList: Product[], args: boolean): Product[] {
+
+        if (args === false) {
+
+
+            return productList.sort(this.sortAZ);
+        } else {
+            function compare(a: Product, b: Product) {
+                if (a.name > b.name) {
+                    return -1;
+                }
+                if (a.name < b.name) {
+                    return 1;
+                }
+                return 0;
+            }
+
+            return productList.sort(compare);
+        }
+    }
+
+     sortAZ(a: Product, b: Product): number {
+        if (a.name.toLocaleLowerCase() < b.name.toLocaleLowerCase()) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    }
 }
